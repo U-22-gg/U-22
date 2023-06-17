8c69214ecc10a9f8b5fc85c510cc021df1a99313
ozu <ficus.y.a.dracaena@gmail.com>
2023-06-17 13:42:16 +0900

    report commit

--- lib/src/screens/report_page.dart
+++ lib/src/screens/report_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ReportPageScreen extends StatelessWidget {
   const ReportPageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _auth = FirebaseAuth.instance;
+    final _firestore = FirebaseFirestore.instance;
+    final uid = _auth.currentUser?.uid ?? '';
+
     return DefaultTabController(
       initialIndex: 0, // 最初に表示するタブ
       length: 2, // タブの数
@@ -28,10 +34,52 @@ class ReportPageScreen extends StatelessWidget {
             labelStyle: TextStyle(fontSize: 15.0),
           ),
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: <Widget>[
             Center(
-              child: Text('マイレポート', style: TextStyle(fontSize: 32.0)),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _firestore
+                    .collection('transaction')
+                    .where('user_id', isEqualTo: uid)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+
+                  final transactions = snapshot.data!.docs;
+                  Map<String, int> totals = {};
+                  int overallTotal = 0;
+                  for (var transaction in transactions) {
+                    final category =
+                        transaction['category'] ?? transaction['expenses'];
+                    final Map<String, dynamic>? data =
+                        transaction.data() as Map<String, dynamic>?;
+                    final price = data != null && data.containsKey('price')
+                        ? transaction.get('price') as int
+                        : 0;
+                    if (totals.containsKey(category)) {
+                      totals[category] = (totals[category] ?? 0) + price;
+                    } else {
+                      totals[category] = price;
+                    }
+                    overallTotal += price;
+                  }
+
+                  return ListView(
+                    children: totals.entries.map((entry) {
+                      return ListTile(
+                        title: Text('${entry.key}: ${entry.value}'),
+                      );
+                    }).toList()
+                      ..add(
+                        ListTile(
+                          title: Text('合計: $overallTotal'),
+                        ),
+                      ),
+                  );
+                },
+              ),
             ),
             Center(
               child: Text('使い方', style: TextStyle(fontSize: 32.0)),
